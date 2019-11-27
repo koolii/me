2dd44ddd76642fd5e1231f3428a15b79082ad77d
koolii <koolii0909@gmail.com>
2019-11-27 02:45:55 +0100

    add react storybook. (storybook doesn't work well. action wasn't be fired

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: #454545;
+  color: #fff;
+`;
+
+const SimpleButton = () => <Button>BUTTON</Button>;
+
+export default SimpleButton;
